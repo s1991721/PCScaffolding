19690f9779a958672a382e8bc4d4fa063211fa1c
Jef <278971585@qq.com>
2020-05-08 21:27:46 +0800

    depend on elementUI;center login template

--- src/main.js
+++ src/main.js
@@ -4,6 +4,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+//使用elementUI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+
+import 'normalize.css/normalize.css'
+
+// 自定义css
+import './styles/index.scss'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
